caaeed4a729c8cbdc07795017ef9fbb7cbdbcf18
Aleksandra Rybinska <rybinska.aleksandra@gmail.com>
2023-05-26 23:16:00 +0200

    lotties updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,12 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
-// @ts-ignore
-import Lottie from "react-lottie";
+import Lottie from "lottie-react";
+import hearts from "../lotties/hearts.json";
+import flowers from "../lotties/flowers.json";
 
 export default function Home() {
   const [open, setOpen] = useState(false);
-
-  const hearts = {
-    loop: true,
-    autoplay: true,
-    animationData: require("../lotties/hearts.json"),
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  const flowers = {
-    loop: true,
-    autoplay: true,
-    animationData: require("../lotties/flowers.json"),
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
 
   return (
     <>
@@ -33,28 +17,35 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div
-      className="card"
-      onClick={() => setOpen((prev) => !prev)}
-      style={{ transform: open ? "rotate(-5deg)" : "unset" }}
-    >
-      <div
-        className="outside"
-        style={{ transform: open ? "rotateY(-130deg)" : "unset" }}
+        className="card"
+        onClick={() => setOpen((prev) => !prev)}
+        style={{ transform: open ? "rotate(-5deg)" : "unset" }}
       >
-        <div className="front">
-          <div className="front-content">
-            <Lottie options={flowers} className="lottie-flowers" />
-            <h1>Z okazji Twojego święta</h1>
-            <h1 className="card-title">Mamo</h1>
+        <div
+          className="outside"
+          style={{ transform: open ? "rotateY(-130deg)" : "unset" }}
+        >
+          <div className="front">
+            <div className="front-content">
+              <div className="lottie-container">
+                <Lottie animationData={flowers} className="flowers" />
+                <h1 className="card-title">Mamo</h1>
+              </div>
+              <h1>Z okazji Twojego święta</h1>
+            </div>
           </div>
+          <div className="back"></div>
         </div>
-        <div className="back"></div>
+        <div className="inside" style={{ left: open ? "49.8%" : "50%" }}>
+          <Lottie animationData={hearts} />
+          <h1 className="inside-text">
+            Wszystkiego <br />
+            co
+            <br />
+            najlepsze!
+          </h1>
+        </div>
       </div>
-      <div className="inside" style={{left: open ? "49.8%" : "50%"}}>
-        <Lottie options={hearts} className="lottie-hearts" />
-        <h1 className="inside-text">Wszystkiego <br />co<br />najlepsze!</h1>
-      </div>
-    </div>
     </>
   );
 }
